51ffafa5cf113b397d0a96eea390c268873526fb
Matthew Jones <matthew.jones@pirum.com>
2023-12-01 09:56:24 +0000

    Day 1 - solution

--- src/main/scala/day1/Day1.scala
+++ src/main/scala/day1/Day1.scala
@@ -6,5 +6,25 @@ import scala.io.Source
 object Day1:
   def readInput(path: String): Chunk[String] = Chunk
     .fromIterator(Source.fromResource(path).getLines())
-  def part1(input: Chunk[String]): Int = ???
-  def part2(input: Chunk[String]): Int = ???
+
+  def part1(input: Chunk[String]): Int =
+    input.map(firstLastNumber).sum
+  def part2(input: Chunk[String]): Int =
+    input.map(replaceWithNumberPad).map(firstLastNumber).sum
+
+  private def firstLastNumber(line: String) =
+    val numbers = line.filter(_.isDigit)
+    s"${numbers.head}${numbers.last}".toInt
+
+  private def replaceWithNumberPad(line: String) =
+    line
+      .replaceAll("one", "one1one")
+      .replaceAll("two", "two2two")
+      .replaceAll("three", "three3three")
+      .replaceAll("four", "four4four")
+      .replaceAll("five", "five5five")
+      .replaceAll("six", "six6six")
+      .replaceAll("seven", "seven7seven")
+      .replaceAll("eight", "eight8eight")
+      .replaceAll("nine", "nine9nine")
+      .replaceAll("zero", "zero0zero")
--- src/test/scala/day1/Day1Spec.scala
+++ src/test/scala/day1/Day1Spec.scala
@@ -8,18 +8,18 @@ object Day1Spec extends ZIOSpecDefault:
   def spec = suite("Day1Spec")(
     test("part1 example") {
       val input = Day1.readInput("day1/example.txt")
-      assert(Day1.part1(input))(equalTo(1010299))
+      assert(Day1.part1(input))(equalTo(142))
     },
     test("part1") {
       val input = Day1.readInput("day1/input.txt")
-      assert(Day1.part1(input))(equalTo(1010299))
+      assert(Day1.part1(input))(equalTo(55607))
     },
     test("part2 example") {
-      val input = Day1.readInput("day1/example.txt")
-      assert(Day1.part2(input))(equalTo(1010299))
+      val input = Day1.readInput("day1/example2.txt")
+      assert(Day1.part2(input))(equalTo(281))
     },
     test("part2") {
       val input = Day1.readInput("day1/input.txt")
-      assert(Day1.part2(input))(equalTo(1010299))
+      assert(Day1.part2(input))(equalTo(55291))
     }
   )
